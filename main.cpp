82a000a257efd93c6257f099a62e730727c4d238
Nikita Yakush <yachkuch@gmail.com>
2024-03-03 13:44:28 +0300

    refacoring

--- main.cpp
+++ main.cpp
@@ -2,7 +2,29 @@
 #include "Matrix.h"
 
 int main(int, char**){
-    Matrix<int ,-1> matr;
-    matr.size();
     std::cout << "Hello, from Matrix!\n";
+    Matrix<int ,-1> matr;
+    matr[0][0] = 0;
+    matr[1][1] = 1;
+    matr[2][2] = 2;
+    matr[3][3] = 3;
+    matr[4][4] = 4;
+    matr[5][5] = 5;
+    matr[6][6] = 6;
+    matr[7][7] = 7;
+    matr[8][9] = 8;
+    matr[9][9] = 9;
+    matr[0][9] = 9;
+    matr[1][8] = 8;
+    matr[2][7] = 7;
+    matr[3][6] = 6;
+    matr[4][5] = 5;
+    matr[5][4] = 4;
+    matr[6][3] = 3;
+    matr[7][2] = 3;
+    matr[8][1] = 1;
+    matr[9][0] = 0;
+    std::cout <<  matr [0][0] << '\n';
+    std::cout <<  matr.size() << '\n';
+    return 0;
 }
--- matrix/Matrix.h
+++ matrix/Matrix.h
@@ -1,23 +1,71 @@
 #ifndef MATRIX
 #define MATRIX
-
+#include <map>
 ///////////////////////////////////////////////////////////
 /// @brief Matrix
 ///
-template <typename T, int size_m>
-class Matrix {
-public:
-    int size ()
+template <typename T, int size_m = -1>
+class Matrix
+{
+    class Iterator
     {
+        using data = std::map<int, T>;
+        data *datas;
+        friend class Matrix;
+        void setData(data *dataVal)
+        {
+            Iterator::datas = dataVal;
+        }
+    public:
+        T & operator[](int i)
+        {
+            if(datas == nullptr) throw std::out_of_range("Matrix is empty");
+            return (*datas)[i];
+        }
+    };
+
+public:
+    Matrix(std::initializer_list<std::initializer_list<T>> list)
+    {
+        int i = 0,j = 0,main_count = 0;
+        for(const auto &row : list)
+        {
+            j = 0;
+            for(const auto &elem : row)
+            {
+                (*this)[i][j] = elem;
+                j++;
+                main_count++;
+            }
+            i++;
+        }
+        this->m_size = main_count;
+    }
+    Matrix()
+    {
+        m_iterator.setData(nullptr);
+    }
+    int size()
+    {
+        this->m_size = 0;
+        for(const auto &row : m_matrix)
+        {
+            this->m_size += row.second.size();
+        }
         return this->m_size;
     };
 
+    Iterator operator[](int i)
+    {
+        m_iterator.setData(&this->m_matrix[i]);
+        return m_iterator;
+    }
+
 private:
     int m_size = 0;
-
-    class Iterator {
-        friend class Matrix;
-    };
+    std::map<int, std::map<int, T>> m_matrix;
+    friend class Iterator;
+    Iterator m_iterator;
 };
 
 #endif // __MATRIX_H_IARA837CJS5Y__
