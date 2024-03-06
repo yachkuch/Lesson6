a7050cfe99fc0ee7a1597e2cd9891ad1f02b1166
Nikita Yakush <yachkuch@gmail.com>
2024-03-06 22:08:18 +0300

    update

--- main.cpp
+++ main.cpp
@@ -4,29 +4,13 @@
 int main(int, char**){
     std::cout << "Hello, from Matrix!\n";
     Matrix<int ,-1> matr;
-    matr[0][0] = 0;
-    matr[1][1] = 1;
-    matr[2][2] = 2;
-    matr[3][3] = 3;
-    matr[4][4] = 4;
-    matr[5][5] = 5;
-    matr[6][6] = 6;
-    matr[7][7] = 7;
-    matr[8][9] = 8;
-    matr[9][9] = 9;
-    matr[0][9] = 9;
-    matr[1][8] = 8;
-    matr[2][7] = 7;
-    matr[3][6] = 6;
-    matr[4][5] = 5;
-    matr[5][4] = 4;
-    matr[6][3] = 3;
-    matr[7][2] = 2;
-    matr[8][1] = 1;
-    matr[9][0] = 0;
+    for (int i = 0; i < 10; i++) {
+    matr[i][i] = i;
+    matr[i][9 - i] = 9 - i;
+}
     std::cout <<  matr [1][1] << '\n';
     std::cout <<  matr.size() << '\n';
-    std::cout<<"_________________________________________________________________\n";
+    std::cout<<"_____________________full matr__________________\n";
     for(const auto& row : matr)
     {
 
